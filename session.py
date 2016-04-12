1c1f348c428e98e6a717ff3fc754705eb14c7903
tvaisanen <vaisanen.toni@gmail.com>
2016-04-12 21:59:49 +0300

    start_udp_messaging()

--- session.py
+++ session.py
@@ -2,7 +2,7 @@ from socket import socket, gethostname
 from socket import AF_INET, SOCK_DGRAM, SOCK_STREAM
 import argparse
 
-""" TODO: instructions to use with argparse"""
+""" TODO: instructions how to use the script with argparse"""
 
 
 class Session:
@@ -26,23 +26,27 @@ class Session:
         try:
             print("connecting to {}:{}".format(target_host, 10000))
             self.tcp_sock.connect((target_host, 10000))
-        except OSError as e:
+        except Exception as e:
             print(str(e))
         self.tcp_sock.send(self.init_msg)
         recv = self.tcp_sock.recv(1024)
 
         # next line is a quick fix to clean up received message from escapes
-        self.host_port = recv.decode('utf-8').split(' ')[1].split('\r')[0]
+        self.target_port = int(recv.decode('utf-8').split(' ')[1].split('\r')[0])
         print('Received msg: {}.'.format(recv))
-        print('Host listens on port: {}.'.format(self.host_port))
+        print('Host listens on port: {}.'.format(self.target_port))
 
         # close connection
         self.tcp_sock.close()
         print(
             'Clients UDP port: {} has been sent.\nClosing TCP-socket..'.format(self.udp_listener_port))
 
-    def start_udp_messaging():
-        pass
+    def start_udp_messaging(self):
+        self.udp_sender_sock = socket(AF_INET, SOCK_DGRAM)
+        smalltalk = "Hello\r\n".encode('utf-8')
+        self.udp_sender_sock.sendto(smalltalk, (self.target_host, self.target_port))
+        print('sent: "Hello\\r\\n".encode(\'utf-8\')')
+        print(self.udp_listener_sock.recvfrom(128))
 
 
 if __name__ == '__main__':
