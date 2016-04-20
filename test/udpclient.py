662c07073deba18a318ae851b56a3c2c0a38ab55
tvaisanen <vaisanen.toni@gmail.com>
2016-04-20 07:32:17 +0300

    Deletet unrelevant files

--- test/udpclient.py
+++
@@ -1,5 +0,0 @@
-import socket
-
-udp_sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_sender.sendto('Hello'.encode('utf-8'), (socket.gethostname(), 10010))
-print('Hello sended to {}.'.format(socket.gethostname()))
