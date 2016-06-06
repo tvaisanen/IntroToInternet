f3fa8650a08a76fbc7ba13eba85b6288e29ed0de
Toni Väisänen <vaisanen.toni@gmail.com>
2016-06-06 13:04:34 +0300

    Delete simple_udp_server.py

--- simple_udp_server.py
+++
@@ -1,7 +0,0 @@
-from socket import socket, AF_INET, SOCK_DGRAM
-
-sock = socket(AF_INET, SOCK_DGRAM)
-sock.bind(('', 12345))
-while True:
-	data = sock.recvfrom(70)
-	print('data: {}.\n'.format(data))
